47a7b9fd2845daeb5b0d8cd923990592a475454d
aswin7712 <aswin557712@gmail.com>
2026-02-05 00:22:26 +0530

    Add files via upload

---
+++ 5.9a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int x,y;
+    printf("enter value of x");
+    scanf("%d",&x);
+    if(x>=0)
+    {
+        if(x==0)
+        {
+            y=0;
+        }
+        else
+        {
+            y=1;
+        }
+    }
+    else
+    {
+        y=-1;
+    }
+    printf("%d",y);
+}
